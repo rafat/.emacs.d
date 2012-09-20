faf8e6c0214f076845b475691ebfb3b222d8e0e5
Rafat Hussain <rafat.hsn@gmail.com>
2012-09-20 06:58:28 +0530

    initial commit

---
+++ init.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-default-init t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-to-list 'load-path "~/.emacs.d" t)
+(require 'go-mode-load)
+
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
+;; auto-complete
+(require 'go-autocomplete)
+(require 'auto-complete-config)
