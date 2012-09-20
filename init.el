b97edbebda6806ab77bf39e4036207769b4955d4
Rafat Hussain <rafat.hsn@gmail.com>
2012-09-20 18:20:18 +0530

    commit themes

--- init.el
+++ init.el
@@ -1,15 +1,4 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-default-init t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 (add-to-list 'load-path "~/.emacs.d" t)
 (require 'go-mode-load)
 
@@ -20,3 +9,6 @@
 ;; auto-complete
 (require 'go-autocomplete)
 (require 'auto-complete-config)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized-light t)
